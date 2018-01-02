613042759266b84d8bffc357333eeeba2dad7696
Callum Rogers <callumr@palantir.com>
2018-01-02 15:12:32 +0000

    Get info out of matches

--- src/typedHoles/util.ts
+++ src/typedHoles/util.ts
@@ -1,3 +1,17 @@
-export function isTypeHole(message: string) {
-return message.match(/Found hole:.* :: .*\n/).length > 0
+interface TypedHole {
+  name: string;
+  type: string;
+}
+
+export function isTypeHole(message: string): TypedHole | null {
+  const match = /Found hole: (.*) :: (.*)\n/.exec(message);
+
+  if (match == null) {
+    return null;
+  }
+
+  return {
+    name: match[1],
+    type: match[2]
+  }
 }
--- unitTest/typeHoles.test.ts
+++ unitTest/typeHoles.test.ts
@@ -1,15 +1,15 @@
 import {expect} from 'chai'
 
-import * as typeHoles from '../src/typedHoles/util';
+import * as typedHoles from '../src/typedHoles/util';
 
 // Defines a Mocha test suite to group tests of similar kind together
 describe("Type Hole Tests", () => {
 
     // Defines a Mocha unit test
     it("should identify a type hole", () => {
-        const typeHole = `
+        const typedHole = `
 [ghcmod]
-• Found hole: _ :: Data.Text.Internal.Lazy.Text -> IO ()
+• Found hole: _name :: Data.Text.Internal.Lazy.Text -> IO ()
 • In the expression: _
   In the expression: _ $ ppllvm myModule
   In an equation for ‘printModule’: printModule = _ $ ppllvm myModule
@@ -18,6 +18,19 @@ describe("Type Hole Tests", () => {
       (bound at /private/var/folders/gx/7lt1gq996118d06q7g9ft7dd080grz/T/ghc-mod96474/Experiment96473-6.hs:28:1)
         `;
 
-        expect(typeHoles.isTypeHole(typeHole)).to.be.true
+        expect(typedHoles.isTypeHole(typedHole)).to.be.deep.equal({
+            name: '_name',
+            type: 'Data.Text.Internal.Lazy.Text -> IO ()'
+        })
+    });
+
+    it("should not identify a normal error as a type hole", () => {
+        const normalError = `
+[ghcmod]
+Variable not in scope:
+    foobar :: Data.Text.Internal.Lazy.Text -> IO ()
+        `;
+
+        expect(typedHoles.isTypeHole(normalError)).to.be.null
     });
 });
